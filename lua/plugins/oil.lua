0cc9d4afcc0d0573456498d76f7b3b5c7589e1dc
Will Robinson <william.ellis.robinson@gmail.com>
2025-03-07 08:35:39 +0000

    feat: added oil plugin for updating file tree as a buffer

---
+++ lua/plugins/oil.lua
@@ -0,0 +1,20 @@
+return {
+    'stevearc/oil.nvim',
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    lazy = false,
+    config = function()
+        require("oil").setup({
+            default_file_explorer = false,
+            columns = {
+                "icon",
+                "permissions",
+                "size",
+            },
+            view_options = {
+                show_hidden = true,
+            }
+        })
+
+        vim.keymap.set("n", "-", "<CMD>Oil<CR>")
+    end
+}
