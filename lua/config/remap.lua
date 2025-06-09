6ba2f6da87a3decea960b721b6d01c9020e5d859
Will Robinson <william.ellis.robinson@gmail.com>
2025-06-09 21:32:00 +0100

    refactor: reverted <C-c> to just :wqa

--- lua/config/remap.lua
+++ lua/config/remap.lua
@@ -9,8 +9,8 @@ vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 
-vim.keymap.set("n", "<C-c>", ":bufdo if &buftype == '' && &modified | write | endif | qa!<CR>")
-vim.keymap.set("i", "<C-c>", "<Esc>:bufdo if &buftype == '' && &modified | write | endif | qa!<CR>")
+vim.keymap.set("n", "<C-c>", ":wqa<CR>")
+vim.keymap.set("i", "<C-c>", "<Esc>:wqa<CR>")
 
 vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
