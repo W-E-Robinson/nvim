7498ec39f5eb5af4f0b5f0a1f7293a742603b637
Will Robinson <william.ellis.robinson@gmail.com>
2025-07-12 15:28:27 +0100

    feat: updated color to be able to alternate between dark and light mode upon leader ics docs: added toggle of light and dark mode to docs

--- lua/plugins/color.lua
+++ lua/plugins/color.lua
@@ -10,6 +10,16 @@ return {
             }
         });
 
-        vim.cmd("colorscheme nightfox")
+        local current_colorscheme = "duskfox"
+        vim.cmd(string.format("colorscheme %s", current_colorscheme))
+
+        vim.keymap.set("n", "<leader>ics", function()
+            if current_colorscheme == "duskfox" then
+                current_colorscheme = "dayfox"
+            else
+                current_colorscheme = "duskfox"
+            end
+            vim.cmd(string.format("colorscheme %s", current_colorscheme))
+        end)
     end
 }
