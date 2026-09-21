ed87a266ea541e41b8acb0f7b3bd3d3402dab319
Will Robinson <william.ellis.robinson@gmail.com>
2026-09-21 20:09:15 +0100

    feat: removed peek and updated markdown-preview setup

--- lua/plugins/markdown-preview.lua
+++ lua/plugins/markdown-preview.lua
@@ -1,10 +1,16 @@
--- :call mdkp#util#install()
 return {
     "iamcco/markdown-preview.nvim",
     cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
     ft = { "markdown" },
-    build = function() vim.fn["mkdp#util#install"]() end,
-        keys = {
+    -- Calling mkdp#util#install() here fails with E117: the plugin is lazy-loaded,
+    -- so its autoload files aren't on runtimepath when the build task runs.
+    -- Invoke the install script directly instead; it no-ops if the binary is current.
+    build = function(plugin)
+        local package_json = plugin.dir .. "/package.json"
+        local version = vim.fn.json_decode(table.concat(vim.fn.readfile(package_json), "")).version
+        vim.fn.system({ "bash", plugin.dir .. "/app/install.sh", "v" .. version })
+    end,
+    keys = {
         {
             "<leader>mp",
             "<cmd>MarkdownPreviewToggle<CR>",
